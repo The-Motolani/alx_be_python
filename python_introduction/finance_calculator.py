200529bb5f97fe55f30bfa94b1d69badf92f2271
Motolani <alebiosumotolani@gmail.com>
2025-09-12 23:46:08 +0100

    updated code to meet checker requirements

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,12 +1,12 @@
 # This program calculates your monthly savings and projects your annual savings with a 5% interest rate.
 # Get user's monthly income and expenses
-user_monthly_income = int(input("Enter your monthly income: "))
-user_monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 # Calculate monthly savings
-user_monthly_savings = user_monthly_income - user_monthly_expenses
+monthly_savings = monthly_income - monthly_expenses
 # To calculate annual savings
 # (Projected Savings = Monthly Savings * 12 + (Monthly Savings * 12 * 0.05))
-user_annual_savings = user_monthly_savings * 12 + (user_monthly_savings * 12 * 0.05)
+annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 # Display the results
 print(f"Your monthly savings are: ${user_monthly_savings}.")
