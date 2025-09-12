4cf9141aeca7e88d7209a1e5ed65b5bf8e63fa22
Motolani <alebiosumotolani@gmail.com>
2025-09-12 23:03:12 +0100

    a Python program tha calculates user age based on input

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+# This program assumes the year is 2023 and calculates how old you will be in the year 2050 based on your current age.
+# requests user input for current age.
+user_current_age = int(input("Enter your current age: "))
+# Calculate future age in 2050
+user_future_age = user_current_age + 27
+# Display the result
+print(f"In 2050, you will be {user_future_age} years old.")
