049fe86d12ff289eff483d983690362cc92713fe
Motolani <alebiosumotolani@gmail.com>
2025-09-18 14:35:05 +0100

    a print that does the multiplication of users iput

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,3 +1,6 @@
+# request user input
 number = int(input("Enter a number to see its multiplication table:"))
+# iterate through numbers 1 - 10 with i representing iterative variable
 for i in range(1, 11):
+    # for every iterative variable of 1- 10 print formatted string of number * iterative variable on new line
     print(f"{number} * {i} = {number * i}")
