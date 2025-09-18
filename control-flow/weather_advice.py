81d1c7563bf3eeb38b512c17923e94d385241f6f
Motolani <alebiosumotolani@gmail.com>
2025-09-18 12:07:18 +0100

    a python script that takes input to give weather advice

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,11 @@
 #ask users for weather details
-weather = input("What's the weather like today? (sunny/rainy/cold):")
+weather = input("What's the weather like today? (sunny/rainy/cold):").lower()
 # give appropriate respone for inputted weather
-if weather.lower() == "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif weather.lower() == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-elif weather.lower() == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I dont have recommendations for this weather.")
