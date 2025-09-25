40ac9b16b10ca78c81c25b53f6f9acbeb00edd5c
Motolani <alebiosumotolani@gmail.com>
2025-09-26 00:04:24 +0100

    changed match statements to conditionals

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -4,17 +4,16 @@ def perform_operation(num1, num2, operation):
         num2 = float(num2)
     except ValueError:
         raise ValueError("Both inputs must be numbers.")
-    match operation:
-        case 'add':
-            return num1 + num2
-        case 'subtract':
-            return num1 - num2
-        case 'multiply':
-            return num1 * num2
-        case 'divide':
-            try:
-                return num1 / num2
-            except ZeroDivisionError:
-                return "Cannot divide by zero."
-        case _:
-            raise ValueError(f"Unsupported operation: {operation}")
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        try:
+            return num1 / num2
+        except ZeroDivisionError:
+            return "Cannot divide by zero."
+    else:
+        raise ValueError(f"Unsupported operation: {operation}")
