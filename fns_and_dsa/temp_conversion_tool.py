d382dbede352cfbd63608e5a8fe84ceb0806ec48
Motolani <alebiosumotolani@gmail.com>
2025-09-26 18:28:03 +0100

    a python script that converts temperature

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,13 +1,13 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
-CELSIUS_TO_FAHRENHEIT_FACTOR =(9/5)
+fahrenheit_to_celsius = (5/9)
+celsius_to_fahrenheit = (9/5)
 
 def convert_to_celsius(fahrenheit):
     """Convert Fahrenheit to Celsius."""
-    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return (fahrenheit - 32) * fahrenheit_to_celsius
 
 def convert_to_fahrenheit(celsius):
     """Convert Celsius to Fahrenheit."""
-    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return (celsius * celsius_to_fahrenheit) + 32
 
 while True:
     try:
