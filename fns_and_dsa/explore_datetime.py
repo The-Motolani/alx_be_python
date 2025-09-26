074eab49de9c093a994bde2d1c63a497d1871213
Motolani <alebiosumotolani@gmail.com>
2025-09-26 15:02:15 +0100

    a script that calculates future date

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,19 @@
+import datetime
+from datetime import date
+
+def  display_current_datetime():
+    current_date = datetime.datetime.today()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(formatted_date)
+    while True:
+        try:
+            future_date = int(input("Enter your number of days: "))
+            break
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+    def calculate_future_date(future_date):
+         future = current_date + datetime.timedelta(days=future_date)
+         return future.strftime("%Y-%m-%d")
+    print(f"Future date after {future_date} days is: {calculate_future_date(future_date)}")
+    
+display_current_datetime()
